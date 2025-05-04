c3e2fe2f4391983994b231452cd0a610ad2c6dfa
Human <53181@edu.raseko.fi>
2025-05-04 20:07:22 +0300

    Added initial migration and defined Member model with firstname and lastname fields

--- django_project/app_members/models.py
+++ django_project/app_members/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255) # Textfield for the fitst name with maximum of 255 characters
+    lastname = models.CharField(max_length=255) # Textfield for the last name with maximum of 255 characters
