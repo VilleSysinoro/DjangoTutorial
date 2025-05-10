29c96788295bb1f4a537d7340ed5b2a83c669d14
afkHuman <villesysinoro@gmail.com>
2025-05-10 21:20:08 +0300

    Updated database with phone and joined_date (null=True).

--- django_project/app_members/models.py
+++ django_project/app_members/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255) # Textfield for the fitst name with maximum of 255 characters
     lastname = models.CharField(max_length=255) # Textfield for the last name with maximum of 255 characters
+    phone = models.IntegerField(null=True) # Integerfield for a phone number
+    joined_date = models.DateField(null=True) # Datefield for a member join date
